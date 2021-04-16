0e9eea1b938236c2bc3ef39082e5a03b884abf6d
milkschen <s93rm6@gmail.com>
2021-04-16 05:46:14 -0400

    completed quick happy summary

--- src/allele_match.py
+++ src/allele_match.py
@@ -1,75 +1,104 @@
+import argparse
+import os
+import sys
+import pandas as pd
 from pysam import VariantFile
 
-fn_panel_vcf = "/net/langmead-bigmem-ib.bluecrab.cluster/storage2/naechyun/reference_flow/resources/1kg_vcf/annotated/chr21.vcf.gz"
-fn_happy_vcf = "/scratch/groups/blangme2/naechyun/imputation_dv/deepvariant_af/HG003.deepvariant_af.happy.vcf.gz"
 
-f_happy_vcf = VariantFile(fn_happy_vcf)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-v', '--vcf',
+        help='path to target happy.out.vcf'
+    )
+    parser.add_argument(
+        '-p', '--panel',
+        help='path to the reference panel vcf'
+    )
+    parser.add_argument(
+        '-o', '--out',
+        help='path to output TSV'
+    )
+    parser.add_argument(
+        '-f', '--force-run', action='store_true',
+        help='Set to force running [False]'
+    )
+    args = parser.parse_args()
+    return args
 
-cycle = 0
-outcome_tmp = {}
-outcome = {
-    'SNP':{
-        'TRUTH.TOTAL': 0,
-        'TRUTH.TP': 0,
-        'TRUTH.FN': 0,
-        'QUERY.TOTAL': 0,
-        'QUERY.FP': 0,
-        'QUERY.UNK': 0,
-        'FP.gt': 0
-    },
-    'INDEL':{
-        'TRUTH.TOTAL': 0,
-        'TRUTH.TP': 0,
-        'TRUTH.FN': 0,
-        'QUERY.TOTAL': 0,
-        'QUERY.FP': 0,
-        'QUERY.UNK': 0,
-        'FP.gt': 0
-    }
-}
 
 def update_happy_outcome(bd, bvt, outcome):
     # TRUTH
     if bd[0] == 'TP':
         outcome[bvt[0]]['TRUTH.TP'] += 1
+        outcome[bvt[0]]['TRUTH.TOTAL'] += 1
     elif bd[0] == 'FN':
         outcome[bvt[0]]['TRUTH.FN'] += 1
+        outcome[bvt[0]]['TRUTH.TOTAL'] += 1
     # QUERY
     if bd[1] == 'FP':
         outcome[bvt[1]]['QUERY.FP'] += 1
 
 
-for var in f_happy_vcf.fetch():
-    if var.info.get('Regions'):
-        bvt = []
-        bd = []
-        for sample in var.samples.items():
-            for v_format in sample[1].items():
-                if v_format[0] == 'BD':
-                    bd.append(v_format[1])
-                elif v_format[0] == 'BVT':
-                    bvt.append(v_format[1])
-            #print(f'sample name: {sample[0]}')
-            #print(f'format values: {sample[1].items()}')
-        update_happy_outcome(bd, bvt, outcome)
-        bd = ' '.join(bd)
-        bvt = ' '.join(bvt)
-        for v_type in bvt:
-            if outcome_tmp.get(v_type):
-                if outcome_tmp[bvt].get(bd):
-                    outcome_tmp[bvt][bd] += 1
-                else:
-                    outcome_tmp[bvt][bd] = 1
-            else:
-                outcome_tmp[bvt] = {bd: 1}
-            # stop update if bvt[0] == bvt[1]
-            if all(t == v_type for t in bvt):
-                break
-        #print(bd)
-        if cycle > 0 and cycle % 1000000 == 0:
-            #cycle = 0
-            #input()
-            print(f'Process {cycle} records')
-        cycle += 1
-print(outcome)
-print(outcome_tmp)
+def write_to_tsv(outcome, out_fn):
+    print(outcome)
+    df = pd.DataFrame.from_dict(outcome, orient='index')
+    df['METRIC.Recall'] = df['TRUTH.TP'] / (df['TRUTH.TP'] + df['TRUTH.FN'])
+    df['METRIC.Precision'] = df['TRUTH.TP'] / (df['TRUTH.TP'] + df['QUERY.FP'])
+    df['METRIC.F1_Score'] = 2 / ((1/df['METRIC.Recall']) + (1/df['METRIC.Precision']))
+    df.to_csv(out_fn, sep='\t', float_format='%.6f')
+    print(df)
+
+
+def summarize_accuracy(fn_happy_vcf, fn_summary):
+    ''' Summarize the accuracy of a VCF file. '''
+    cycle = 0
+    fields = {
+        'TRUTH.TOTAL': 0,
+        'TRUTH.TP': 0,
+        'TRUTH.FN': 0,
+        'QUERY.FP': 0,
+    }
+    outcome = {'SNP': fields, 'INDEL': fields.copy()}
+
+    f_happy_vcf = VariantFile(fn_happy_vcf)
+    for var in f_happy_vcf.fetch():
+        # Only check variants in confident regions
+        if var.info.get('Regions'):
+            bvt = []
+            bd = []
+            # happy samples: 'TRUTH' and 'QUERY'
+            for sample in var.samples.items():
+                for v_format in sample[1].items():
+                    if v_format[0] == 'BD':
+                        bd.append(v_format[1])
+                    elif v_format[0] == 'BVT':
+                        bvt.append(v_format[1])
+            update_happy_outcome(bd, bvt, outcome)
+            
+            if cycle > 0 and cycle % 1000000 == 0:
+                print(f'Process {cycle} records')
+            cycle += 1
+
+    write_to_tsv(outcome, fn_summary)
+
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    fn_happy_vcf = args.vcf
+    fn_panel_vcf = args.panel
+    fn_summary = args.out
+    
+    print('Warning: the indel precision and F1_score measurement are not identical to official hap.py',
+           file=sys.stderr)
+    if not args.force_run:
+        if os.path.exists(fn_summary):
+            print(f'File "{fn_summary}" exists. Exit.')
+            df = pd.read_csv(fn_summary, sep='\t')
+            print(df)
+            exit(0)
+
+    summarize_accuracy(fn_happy_vcf, fn_summary)
+
+
